0456c13c1fee08bc986b2d6ff239a540356a5aa9
asiguqa <asiguqa@student.wethinkcode.co.za>
2020-09-16 12:08:00 +0200

    wtc-lms save

--- robot.py
+++ robot.py
@@ -14,17 +14,23 @@ def on_off():
         if command.lower() == 'help':
             help_command()
             continue
+        elif command.lower() == 'forward':
+            print('> ' + robot_name + 'moved forward by ' +move_forward() + ' steps.')
+            continue
         elif command.lower() == 'off':
             print(robot_name + ': Shutting down..')
             break
         else:
             print(robot_name + ': Sorry, I did not understand ' + '\''+ command + '\'.')
             continue
+def move_forward():
+    
 
 def help_command():
     print('I can understand these commands:')
     print('OFF  - Shut down robot')
     print('HELP - provide information about commands')
+    print('FORWARD - move robot forward')
     print()
 
 def robot_start():
