2d96005f28bcbfe5598e82299976c4757b4002f3
asiguqa <asiguqa@student.wethinkcode.co.za>
2020-09-16 11:44:11 +0200

    wtc-lms save

--- robot.py
+++ robot.py
@@ -1,8 +1,35 @@
 
+robot_name ='' 
+def provide_name():
+    """Let's the child provide a name for the robot"""
+    global robot_name
+    robot_name = input('What do you want to name your robot? ')
+    print(robot_name + ': Hello kiddo!')
+
+def on_off():
+    """Allows the child turn the robot on or off"""
+    global robot_name
+    while True:
+        command = input(robot_name + ": What must I do next? ")
+        if command.lower() == 'help':
+            help_command()
+            continue
+        elif command.lower() == 'off':
+            print(robot_name + ': Shutting down..')
+            break
+        else:
+            print(robot_name + ': Sorry, I did not understand ' + '\''+ command + '\'.')
+            continue
+
+def help_command():
+    print('I can understand these commands:')
+    print('OFF  - Shut down robot')
+    print('HELP - provide information about commands')
+    print()
+
 def robot_start():
     """This is the entry function, do not change"""
-    pass
-
-
+    provide_name()
+    on_off()
 if __name__ == "__main__":
     robot_start()
---
+++ test_robot.py
@@ -0,0 +1,6 @@
+import unittest
+import robot
+
+class MyTestCase(unittest.TestCase):
+    def test_on_off(self):
+        self.assertEqual('off',robot.on_off()
