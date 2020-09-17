e09c15097cc9f3e34dcc0e9ff143e95351a92bbe
asiguqa <asiguqa@student.wethinkcode.co.za>
2020-09-17 15:59:36 +0200

    wtc-lms save

--- robot.py
+++ robot.py
@@ -1,5 +1,8 @@
 
-robot_name ='' 
+x = 0
+y = 0
+robot_name =''
+command = ''
 def provide_name():
     """Let's the child provide a name for the robot"""
     global robot_name
@@ -9,13 +12,15 @@ def provide_name():
 def on_off():
     """Allows the child turn the robot on or off"""
     global robot_name
+    global command
     while True:
         command = input(robot_name + ": What must I do next? ")
         if command.lower() == 'help':
             help_command()
             continue
-        elif command.lower() == 'forward':
-            print('> ' + robot_name + 'moved forward by ' +move_forward() + ' steps.')
+        elif 'forward' in command.lower():
+            print(' > ' + robot_name + ' moved forward by ' + str(move_forward()) + ' steps.')
+            print(' > ' + robot_name + ' now at position ' + str(track_position()).replace(' ','') + '.')
             continue
         elif command.lower() == 'off':
             print(robot_name + ': Shutting down..')
@@ -24,8 +29,18 @@ def on_off():
             print(robot_name + ': Sorry, I did not understand ' + '\''+ command + '\'.')
             continue
 def move_forward():
-    
+    global y
+    if ' ' in command:
+        new_command = command.split(' ')
+        y += int(new_command[1])
+        return int(new_command[1])
+    pass
 
+def track_position():
+    global x
+    global y
+
+    return x,y
 def help_command():
     print('I can understand these commands:')
     print('OFF  - Shut down robot')
